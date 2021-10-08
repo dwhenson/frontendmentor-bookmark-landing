11ae18d369afe78b594e144f9e6ee56c0c9c2f83
dwhenson <dave.henson@gmail.com>
2021-10-08 17:22:39 +0300

    🐛 FIX: Move tab JS to index file

--- src/js/utils/elements.js
+++ src/js/utils/elements.js
@@ -1,5 +1,11 @@
+// Form elements
 export const form = document.querySelector("#form");
 export const submitForm = document.querySelector("#submit");
 export const emailInput = document.querySelector("#email");
 export const errorField = document.querySelector("#error-message");
 export const inputWrapper = document.querySelector(".input-wrapper");
+// Tab elements
+export const tabbed = document.querySelector(".tabbed");
+export const tablist = tabbed.querySelector("ul");
+export const tabs = tablist.querySelectorAll("a");
+export const panels = tabbed.querySelectorAll('[id^="section"]');
